8c74c0ea8e1f912b117dba4b886b594ea15c68e3
0xGavinOwen <gavinowen713@gmail.com>
2024-11-17 02:57:47 +0700

    feat: EMERGENCY WORKING

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -71,7 +71,7 @@ export default function index() {
           address as `0x${string}`, // oracle - address
           questionId as `0x${string}`, // questionId - bytes32
           BigInt(2), // outcomeSlotCount - uint256
-          BigInt(epochSeconds + 10000), // endTime - uint256
+          BigInt(epochSeconds), // endTime - uint256
           BigInt(2), // fee - uint256
         ],
       });
